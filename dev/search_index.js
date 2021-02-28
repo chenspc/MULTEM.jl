a5dc8e0467b58962d3ed5bbe2878d1cd6ae1bcca
Documenter.jl <documenter@juliadocs.github.io>
2021-02-28 20:52:27 +0000

    build based on 5d48bc9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MULTEM","category":"page"},{"location":"#MULTEM","page":"Home","title":"MULTEM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MULTEM]","category":"page"}]
+}
